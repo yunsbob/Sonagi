f95aef9dc210bb7aaf1208d416c73d3573d94b7a
윤민재 <w124564@naver.com>
2023-07-25 15:20:28 +0900

    [S09P12B207-216] feat: spring boot 초기 세팅

---
+++ backend-web/src/test/java/com/fa/sonagi/SonagiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fa.sonagi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonagiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
