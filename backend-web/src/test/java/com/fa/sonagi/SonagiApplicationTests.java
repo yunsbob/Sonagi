701910db0d5fdb08bfbbc6363f3c78f216553ec5
윤민재 <w124564@naver.com>
2023-07-26 16:16:22 +0900

    [S09P12B207-208] feat: redis 초기 세팅

--- backend-web/src/test/java/com/fa/sonagi/SonagiApplicationTests.java
+++ backend-web/src/test/java/com/fa/sonagi/SonagiApplicationTests.java
@@ -1,9 +1,11 @@
 package com.fa.sonagi;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Transactional
 class SonagiApplicationTests {
 
 	@Test
