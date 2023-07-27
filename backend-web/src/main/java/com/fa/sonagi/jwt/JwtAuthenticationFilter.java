043ff0d52a08dac2e81db39e5618eaafb702a303
YunsBob <w124564@naver.com>
2023-07-28 00:12:59 +0900

    [S09P12B207-202] feat: JwtAuthenticationFilter 추가

---
+++ backend-web/src/main/java/com/fa/sonagi/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.fa.sonagi.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        String jwtToken = resolveToken((HttpServletRequest) request);
+        log.info(jwtToken);
+
+        // 토큰 유효성 검사
+        if (jwtToken != null && jwtTokenProvider.validateToken(jwtToken)) {
+            String isLogout = redisTemplate.opsForValue().get(jwtToken);
+            if (ObjectUtils.isEmpty(isLogout)) {
+                Authentication auth = jwtTokenProvider.getAuthentication(jwtToken);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    // Request Header에서 토큰 추출
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7);
+        }
+
+        return null;
+    }
+
+}
