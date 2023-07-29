fb12e9e952ff2ac8aff64963e44fb9ba50ae8f73
hellobaemin <hellobaemin@gmail.com>
2023-07-30 05:13:00 +0900

    [S09P12B207-254] feat : Button, CategoryBar, RecordBar 기능 추가

---
+++ frontend-web/src/components/atoms/Button/Button.styles.ts
@@ -0,0 +1,53 @@
+import styled, { css } from 'styled-components';
+import { ButtonProps } from './Button';
+
+const getVariantStyling = (variant: Required<ButtonProps>['variant']) => {
+  const style = {
+    default: css``,
+    register: css`
+      border: 1px rgba(70, 70, 70, 0.6) solid;
+      background: ${({ theme }) => theme.color.gray3};
+      border: 1px ${({ theme }) => theme.color.gray2} solid;
+      color: ${({ theme }) => theme.color.black3};
+      box-shadow: 0px 8px 21px 0px rgba(0, 0, 0, 0.16);
+      font-size: 22px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    `,
+    record: css`
+      border-radius: 1.375rem;
+      border: 1px solid var(--grey-3, #d7d7d7);
+      background: var(--white-1, #fff);
+    `,
+  };
+
+  return style[variant];
+};
+
+const getSizeStyling = (size: Required<ButtonProps>['size']) => {
+  const style = {
+    small: css`
+      width: 4.375rem;
+      height: 2.25rem;
+      flex-shrink: 0;
+    `,
+    medium: css`
+      width: 307px;
+      height: 62px;
+      flex-shrink: 0;
+    `,
+  };
+  return style[size];
+};
+
+const Button = styled.button<ButtonProps>`
+  cursor: pointer;
+  border-radius: 22px;
+  font-family: Happiness Sans;
+  text-align: center;
+  ${({ variant = 'default' }) => getVariantStyling(variant)}
+  ${({ size = 'small' }) => getSizeStyling(size)}
+`;
+
+export { Button };
--- frontend-web/src/pages/MainPage/MainPage.tsx
+++ frontend-web/src/pages/MainPage/MainPage.tsx
@@ -1,15 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import { Background } from '@/components/atoms/Background/Background.styles';
 import backgroundGradient from '@/assets/images/background-gradient.png';
-import { Text } from '@/components/atoms/Text/Text.styles';
 import TabBar from '@/components/organisms/TabBar/TabBar';
+import BabyBar from '@/components/molecules/BabyBar/BabyBar';
 
 const MainPage = () => {
   return (
     <Background background={backgroundGradient}>
       <div style={{ backgroundColor: 'white' }}>
         <header>
-          <Text size="medium1">(임시) 이지은 // 이지금</Text>
+          <BabyBar></BabyBar>
         </header>
       </div>
       <Outlet />
