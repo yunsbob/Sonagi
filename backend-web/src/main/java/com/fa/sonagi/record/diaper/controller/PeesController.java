27180a6087fc701f144f589562188964c3d44374
minseo0421 <k4496k@naver.com>
2023-07-30 16:55:32 +0900

    [S09P12B207-198] rename: pee controller mapping 수정

--- backend-web/src/main/java/com/fa/sonagi/record/diaper/controller/PeesController.java
+++ backend-web/src/main/java/com/fa/sonagi/record/diaper/controller/PeesController.java
@@ -10,31 +10,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/pees")
 @RestController
 @RequiredArgsConstructor
 public class PeesController {
 
   private final PeesService peesService;
 
-  @PostMapping("/pees")
+  @PostMapping
   public ResponseEntity<?> registPees(@RequestBody PeesPostDto peesPostDto) {
     peesService.registPees(peesPostDto);
-
     return ResponseEntity.ok().build();
   }
 
-  @PutMapping("/pees")
+  @PutMapping
   public ResponseEntity<?> updatePees(@RequestBody PeesPutDto peesPutDto) {
     peesService.updatePees(peesPutDto);
     return ResponseEntity.ok().build();
   }
 
-  @DeleteMapping("/pees/{peeId}")
+  @DeleteMapping("/{peeId}")
   public ResponseEntity<?> deletePees(@PathVariable Long peesId) {
     peesService.deletePeesById(peesId);
-
     return ResponseEntity.ok().build();
   }
 }
