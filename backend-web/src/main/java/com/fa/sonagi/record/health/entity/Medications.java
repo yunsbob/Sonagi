2dceb4a76fccac95d26d0452dbbf2c7cc2e98808
minseo0421 <k4496k@naver.com>
2023-07-30 18:09:32 +0900

    [S09P12B207-198] feat: medication Api 개발

---
+++ backend-web/src/main/java/com/fa/sonagi/record/health/entity/Medications.java
@@ -0,0 +1,42 @@
+package com.fa.sonagi.record.health.entity;
+
+import com.fa.sonagi.baby.entity.Baby;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "medication")
+public class Medications {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "medication_id")
+  private Long id;
+
+  @Column(name = "user_id")
+  private Long userId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @Column(name = "baby_id")
+  private Baby baby;
+
+  @Column(name = "created_date")
+  private LocalDate createdDate;
+
+  @Column(name = "created_time")
+  private LocalTime createdTime;
+
+  @Column(name = "memo")
+  private String memo;
+
+  public void updateMedications(LocalTime createdTime, String memo) {
+    this.createdTime = createdTime;
+    this.memo = memo;
+  }
+}
