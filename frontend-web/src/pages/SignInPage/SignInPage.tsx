248a12a792203185ac2635fd7daf655e6e98c62f
leecr1215 <leecr1215@naver.com>
2023-07-30 21:39:01 +0900

    [S09P12B207-257] feat: user state 추가

--- frontend-web/src/pages/SignInPage/SignInPage.tsx
+++ frontend-web/src/pages/SignInPage/SignInPage.tsx
@@ -3,27 +3,13 @@ import orangeBackground from '@/assets/images/background-orange-to-blue.png';
 import { Text } from '@/components/atoms/Text/Text.styles';
 import family from '@/assets/images/img-family.png';
 import { Image } from '@/components/atoms/Image/Image';
-import Input from '@/components/atoms/Input/Input';
 import * as S from '@/pages/SignInPage/SignInPage.style';
 import Back from '@/components/atoms/Back/Back';
 import RegisterField from '@/components/molecules/RegisterField/RegisterField';
-import { useState } from 'react';
-import theme from '@/styles/theme';
-
 const SignInPage = () => {
-  const [disabled, setDisabled] = useState(true);
-
   const placeholder = '이름을 입력하세요';
 
   const alertMessage = '10자 이내로 입력해주세요';
-
-  // const getInputValue = (value: string) => {
-  //   if (value.length > 0) {
-  //     setDisabled(false);
-  //   } else {
-  //     setDisabled(true);
-  //   }
-  // };
 
   return (
     <Background $background={orangeBackground}>
@@ -41,9 +27,6 @@ const SignInPage = () => {
             size="medium"
             placeholder={placeholder}
             alertMessage={alertMessage}
-            $backgroundColor={
-              disabled ? theme.color.gray2 : theme.gradient.orangeBtn
-            }
           />
         </S.SignInPageWrapper>
       </S.SignInPageContainer>
