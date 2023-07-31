48a95dabe1d8e40f3839327d4469596ac34340b6
hellobaemin <hellobaemin@gmail.com>
2023-08-01 08:09:14 +0900

    [S09P12B207-254] feat : block 클릭시 해당 block의 txt 추가

--- frontend-web/src/pages/RecordPage.tsx/RecordPage.tsx
+++ frontend-web/src/pages/RecordPage.tsx/RecordPage.tsx
@@ -4,16 +4,21 @@ import { Text } from '@/components/atoms/Text/Text.styles';
 import CategoryBar from '@/components/molecules/CategoryBar/CategoryBar';
 import RecordBar from '@/components/molecules/RecordBar/RecordBar';
 import RecordBlock from '../../components/molecules/RecordBlock/RecordBlock';
+import { useRecoilValue } from 'recoil';
+import { recordBlocksState } from '@/states/RecordState';
 
 const RecordPage: React.FC = () => {
+  const recordBlocks = useRecoilValue(recordBlocksState);
+
   return (
     <div>
       <CategoryBar></CategoryBar>
 
-      <Text size="headXLarge">기록 블록 1</Text>
-      <Text size="headXLarge">기록 블록 2</Text>
-      <Text size="headXLarge">기록 블록 3</Text>
-
+      {recordBlocks.map((record, index) => (
+        <Text size="headXLarge" key={index}>
+          {record}
+        </Text>
+      ))}
       <RecordBlock color={theme.color.categoryMeal}></RecordBlock>
 
       <RecordBar></RecordBar>
