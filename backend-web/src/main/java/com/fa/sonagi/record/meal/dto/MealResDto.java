245f1cc4a2f2dd1822c643bbb892efc36fcba1a7
윤민재 <w124564@naver.com>
2023-08-01 21:56:10 +0900

    [S09P12B207-329] feat: 식사 카테고리 조회 기능 추가

--- backend-web/src/main/java/com/fa/sonagi/record/meal/dto/MealResDto.java
+++ backend-web/src/main/java/com/fa/sonagi/record/meal/dto/MealResDto.java
@@ -2,11 +2,13 @@ package com.fa.sonagi.record.meal.dto;
 
 import java.sql.Time;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class MealResDto {
 	private Long id;
 	private Long amount;
