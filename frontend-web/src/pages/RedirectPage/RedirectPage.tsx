69a26a1e34511233456dfdc1537e75b43bb0bece
leecr1215 <leecr1215@naver.com>
2023-08-02 22:09:38 +0900

    [S09P12B207-255] feat : 소셜 로그인 링크 연결

--- frontend-web/src/pages/RedirectPage/RedirectPage.tsx
+++ frontend-web/src/pages/RedirectPage/RedirectPage.tsx
@@ -4,12 +4,13 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 const RedirectPage = () => {
   console.log('리다이렉트 페이지 왔다');
-  const navigator = useNavigate();
-  const params = useParams();
 
-  localStorage.clear();
-  localStorage.setItem('accessToken', params.token!);
-  navigator(PATH.SIGNIN);
+  const params = new URLSearchParams(location.search);
+  const accessToken = params.get('accessToken');
+
+  localStorage.setItem('accessToken', accessToken!);
+
+  window.location.href = PATH.SIGNIN;
 
   return <></>;
 };
