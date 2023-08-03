83cd4788f591fc9fddc29b238f436d2766a32704
윤민재 <w124564@naver.com>
2023-08-03 12:28:45 +0900

    [S09P12B207-298] remove: 필요 없는 패키지 삭제

--- backend-web/src/main/java/com/fa/sonagi/baby/entity/Baby.java
+++ backend-web/src/main/java/com/fa/sonagi/baby/entity/Baby.java
@@ -1,22 +1,14 @@
 package com.fa.sonagi.baby.entity;
 
-import com.fa.sonagi.record.meal.entity.BabyFood;
-import com.fa.sonagi.record.meal.entity.BreastFeeding;
-
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.sql.Date;
-
-import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Getter
@@ -31,16 +23,16 @@ public class Baby {
     @Column(name = "baby_id")
     private Long id;
 
-    @Column(name = "birth_date")
-    private Date birthDate;
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
 
-    @Column(name = "gender")
+    @Column(name = "gender", nullable = false, length = 2)
     private String gender;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false, length = 25)
     private String name;
 
-    @Column(name = "code")
+    @Column(name = "code", nullable = false)
     private String code;
 
     @Column(name = "last_meal_time")
@@ -49,10 +41,9 @@ public class Baby {
     @Column(name = "last_diary_time")
     private LocalDateTime lastDiaryTime;
 
-    @Column(name = "deleted_at")
-    private LocalDateTime deletedAt;
+    @Column(name = "deleted_at", nullable = false)
+    private LocalDate deletedAt;
 
-    @ColumnDefault("0")
-    @Column(name = "is_deleted")
-    private Boolean isDeleted;
+    @Column(name = "is_deleted", nullable = false, length = 2)
+    private String isDeleted;
 }
