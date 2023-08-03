40bab185c43db4387bf6f07936544276262dcace
이채림 <leecr1215@naver.com>
2023-08-03 22:32:22 +0900

    [S09P12B207-278] 그래프 페이지에 식사 통계

---
+++ frontend-web/src/components/molecules/CardContent/CardContentText.styles.ts
@@ -0,0 +1,16 @@
+import { styled } from 'styled-components';
+
+const CardContentTextWrapper = styled.div`
+  p {
+    width: fit-content;
+    display: flex;
+    align-items: center;
+    margin-bottom: 0.2rem;
+  }
+  b {
+    margin-left: 5px;
+    margin-right: 1px;
+  }
+`;
+
+export { CardContentTextWrapper };
---
+++ frontend-web/src/components/molecules/CardHeader/CardHeader.style.ts
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+const CardHeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+`;
+
+export { CardHeaderWrapper };
