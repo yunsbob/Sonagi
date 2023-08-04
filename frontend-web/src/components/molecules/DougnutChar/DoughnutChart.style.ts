220f322c2e8c361af6fb0d6f7bb1ec05cb8c2237
이채림 <leecr1215@naver.com>
2023-08-04 16:43:35 +0900

    [S09P12B207-278] feat: 도넛 모양 UI 생성

---
+++ frontend-web/src/components/molecules/DougnutChar/DoughnutChart.style.ts
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components';
+
+const DonutGraphContainer = styled.div`
+  width: 90vw;
+  background-color: white;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export { DonutGraphContainer };
