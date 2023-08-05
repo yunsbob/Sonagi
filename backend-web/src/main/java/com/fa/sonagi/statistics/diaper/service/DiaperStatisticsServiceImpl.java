5e2bd7c0763addaebf77c5b29bce4609f147ba0a
윤민재 <w124564@naver.com>
2023-08-05 17:38:17 +0900

    [S09P12B207-292] style : 주석 및 보기 편하게 변경

--- backend-web/src/main/java/com/fa/sonagi/statistics/diaper/service/DiaperStatisticsServiceImpl.java
+++ backend-web/src/main/java/com/fa/sonagi/statistics/diaper/service/DiaperStatisticsServiceImpl.java
@@ -22,17 +22,23 @@ public class DiaperStatisticsServiceImpl implements DiaperStatisticsService{
 	private final PeeRepository peeRepository;
 	private final PoopRepository poopRepository;
 
+	/**
+	 * 일별 통계 계산
+	 */
 	@Override
 	public DiaperStatisticsResDto getDiaperStatisticsDay(Long babyId, LocalDate createdDate) {
 		DiaperStatisticsResDto diaperStatisticsResDto = new DiaperStatisticsResDto();
 
+		// 데이터 조회
 		List<DiaperStatisticsQueryDto> pees = findPees(babyId, createdDate);
 		diaperStatisticsResDto.setPees(pees);
 		List<DiaperStatisticsQueryDto> poops = findPoops(babyId, createdDate);
 		diaperStatisticsResDto.setPoops(poops);
-		Long peeCnt = findPeeCnt(babyId, createdDate);
+
+		// 횟수 통계
+		Long peeCnt = (long)pees.size();
 		diaperStatisticsResDto.setPeeCnt(peeCnt);
-		Long poopCnt = findPoopCnt(babyId, createdDate);
+		Long poopCnt = (long)poops.size();
 		diaperStatisticsResDto.setPoopCnt(poopCnt);
 
 		createdDate = createdDate.minus(1, ChronoUnit.DAYS);
@@ -41,6 +47,7 @@ public class DiaperStatisticsServiceImpl implements DiaperStatisticsService{
 		Long yesterdayPoopCnt = findPoopCnt(babyId, createdDate);
 
 		Long peePercent, poopPercent, yesterdayPeePercent, yesterdayPoopPercent;
+
 		if (peeCnt >= yesterdayPeeCnt) {
 			if (peeCnt == 0) peePercent = 0L;
 			else peePercent = 100L;
@@ -52,6 +59,9 @@ public class DiaperStatisticsServiceImpl implements DiaperStatisticsService{
 			if (peeCnt == 0) peePercent = 0L;
 			else peePercent = peeCnt * 100 / yesterdayPeeCnt;
 		}
+		diaperStatisticsResDto.setPeePercent(peePercent);
+		diaperStatisticsResDto.setYesterdayPeePercent(yesterdayPeePercent);
+
 		if (poopCnt >= yesterdayPoopCnt) {
 			if (poopCnt == 0) poopPercent = 0L;
 			else poopPercent = 100L;
@@ -63,30 +73,39 @@ public class DiaperStatisticsServiceImpl implements DiaperStatisticsService{
 			if (poopCnt == 0) poopPercent = 0L;
 			else poopPercent = poopCnt * 100 / yesterdayPoopCnt;
 		}
-
-		diaperStatisticsResDto.setPeePercent(peePercent);
-		diaperStatisticsResDto.setYesterdayPeePercent(yesterdayPeePercent);
 		diaperStatisticsResDto.setPoopPercent(poopPercent);
 		diaperStatisticsResDto.setYesterdayPoopPercent(yesterdayPoopPercent);
 
 		return diaperStatisticsResDto;
 	}
 
+	/**
+	 * 소변 일별 데이터 조회
+	 */
 	@Override
 	public List<DiaperStatisticsQueryDto> findPees(Long babyId, LocalDate createdDate) {
 		return peeRepository.findPeeByDay(babyId, createdDate);
 	}
 
+	/**
+	 * 대변 일별 데이터 조회
+	 */
 	@Override
 	public List<DiaperStatisticsQueryDto> findPoops(Long babyId, LocalDate createdDate) {
 		return poopRepository.findPoopByDay(babyId, createdDate);
 	}
 
+	/**
+	 * 소변 일별 횟수 조회
+	 */
 	@Override
 	public Long findPeeCnt(Long babyId, LocalDate createdDate) {
 		return peeRepository.findPeeCnt(babyId, createdDate);
 	}
 
+	/**
+	 * 대변 일별 횟수 조회
+	 */
 	@Override
 	public Long findPoopCnt(Long babyId, LocalDate createdDate) {
 		return poopRepository.findPoopCnt(babyId, createdDate);
