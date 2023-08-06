fbaa7b260d1933f26d4654b57d07c0017d708a2d
박JunePC <ssafy9june@gmail.com>
2023-08-06 17:31:13 +0900

    [S09P12B207-315] feat : React Native 빌드 환경 설정

--- frontend-app/App.tsx
+++ frontend-app/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import WebView from 'react-native-webview';
+import DeviceInfo from 'react-native-device-info';
 
 const App = () => {
-  return <WebView source={{uri: 'https://naver.com'}} />;
+  return (
+    <WebView
+      source={{uri: 'https://sonagi.site'}}
+      userAgent={DeviceInfo.getUserAgent() + '-kwdApp-'}
+    />
+  );
 };
 
 export default App;
