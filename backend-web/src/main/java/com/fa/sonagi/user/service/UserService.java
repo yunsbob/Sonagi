c47423b8c68a064588c2bc45e1045d7b3f33625b
joonho <tjsduq0423@uos.ac.kr>
2023-08-08 22:49:05 +0900

    [S09P12B207-350] feat : token dto 생성  FCM config 설정 및 의존성 추가

--- backend-web/src/main/java/com/fa/sonagi/user/service/UserService.java
+++ backend-web/src/main/java/com/fa/sonagi/user/service/UserService.java
@@ -1,7 +1,5 @@
 package com.fa.sonagi.user.service;
 
-import org.springframework.http.ResponseEntity;
-
 import com.fa.sonagi.user.dto.NameDto;
 
 public interface UserService {
@@ -9,6 +7,4 @@ public interface UserService {
 	void updateName(NameDto nameDto);
 
 	NameDto findName(Long id);
-	// 재발급 로직 필요함.
-	// ResponseEntity<?> reissue(UserTestReqDto.Reissue reissue);
 }
