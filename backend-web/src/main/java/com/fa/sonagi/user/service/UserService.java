de74577c10501b18804fcff9396cfbd79d1072a8
joonho <tjsduq0423@uos.ac.kr>
2023-08-08 21:46:36 +0900

    [S09P12B207-350] feat: userservice 수정

--- backend-web/src/main/java/com/fa/sonagi/user/service/UserService.java
+++ backend-web/src/main/java/com/fa/sonagi/user/service/UserService.java
@@ -1,13 +1,9 @@
 package com.fa.sonagi.user.service;
 
-import org.springframework.http.ResponseEntity;
-
 import com.fa.sonagi.user.dto.NameDto;
 
 public interface UserService {
 	boolean checkEmail(String email) throws Exception;
-
-	ResponseEntity<?> getUserInfo(String email);
 
 	void updateName(NameDto nameDto);
 
