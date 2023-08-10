6177fb0ddc8fe0452f112ab1c263eec8a5cac1b5
윤민재 <w124564@naver.com>
2023-08-10 10:39:24 +0900

    [S09P12B207-336] feat : 시간 관련 설정

--- backend-web/src/main/java/com/fa/sonagi/statistics/diaper/dto/DiaperStatisticsDayForWeekDto.java
+++ backend-web/src/main/java/com/fa/sonagi/statistics/diaper/dto/DiaperStatisticsDayForWeekDto.java
@@ -1,11 +1,11 @@
-package com.fa.sonagi.statistics.common;
+package com.fa.sonagi.statistics.diaper.dto;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class StatisticsTime {
+public class DiaperStatisticsDayForWeekDto {
 	Long startTime;
 	Long passTime;
 }
