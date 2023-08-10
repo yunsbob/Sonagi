3ae8cc1758fc2d76f75fbd9184e4d626cba0ecc9
윤민재 <w124564@naver.com>
2023-08-10 17:57:14 +0900

    [S09P12B207-338] feat : 전날 날짜 추가 및 다음날 날짜 자르기

--- backend-web/src/main/java/com/fa/sonagi/statistics/sleep/service/SleepStatisticsServiceImpl.java
+++ backend-web/src/main/java/com/fa/sonagi/statistics/sleep/service/SleepStatisticsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.fa.sonagi.statistics.sleep.service;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 
@@ -30,6 +31,15 @@ public class SleepStatisticsServiceImpl implements SleepStatisticsService{
 
 		// 데이터 조회
 		List<SleepStatisticsQueryDto> sleeps = sleepRepository.findSleepByDay(babyId, createdDate);
+		sleeps = cutNextTime(sleeps);
+
+		createdDate = createdDate.minus(1, ChronoUnit.DAYS);
+
+		// 전날 수면 데이터 조회
+		List<SleepStatisticsQueryDto> yesterdaySleeps = sleepRepository.findSleepByDay(babyId, createdDate);
+		yesterdaySleeps = cutNextTime(yesterdaySleeps);
+
+		List<SleepStatisticsQueryDto> pastSleeps = sleepRepository.findSleepByDay(babyId, createdDate.minusDays(1));
 
 		// 카드 통계
 		Long sleepTime = sumSleepTime(sleeps);
@@ -38,11 +48,6 @@ public class SleepStatisticsServiceImpl implements SleepStatisticsService{
 		sleepStatisticsResDto.setSleepCnt(cnt);
 		sleepStatisticsResDto.setAllSleepHour(sleepTime / 60);
 		sleepStatisticsResDto.setAllSleepMinute(sleepTime % 60);
-
-		createdDate = createdDate.minus(1, ChronoUnit.DAYS);
-
-		// 하루 전 수면 데이터 조회
-		List<SleepStatisticsQueryDto> yesterdaySleeps = sleepRepository.findSleepByDay(babyId, createdDate);
 
 		// 수면 횟수 통계 계산
 		Long yesterdayCnt = (long)yesterdaySleeps.size();
@@ -70,6 +75,45 @@ public class SleepStatisticsServiceImpl implements SleepStatisticsService{
 	}
 
 	/**
+	 * 퍼센트 계산하기 Long
+	 */
+	public Long getPercent(Long target, Long opponent) {
+		if (target == 0) return 0L;
+		else if (target >= opponent) {
+			return 100L;
+		}
+		else {
+			return target * 100 / opponent;
+		}
+	}
+
+	/**
+	 * 다음날 시간 자르기 ex) 23:00 ~ 01:20 -> 23:00 ~ 23:59
+	 */
+	public List<SleepStatisticsQueryDto> cutNextTime(List<SleepStatisticsQueryDto> sleeps) {
+		for (int i = 0; i < sleeps.size(); i++) {
+			if (sleeps.get(i).getEndTime().isBefore(sleeps.get(i).getCreatedTime())) {
+				sleeps.get(i).setEndTime(LocalTime.of(23, 59));
+			}
+		}
+
+		return sleeps;
+	}
+
+	/**
+	 * 전날 시간 추가하기 ex) 23:00 ~ 01:20 -> 00:00 ~ 01:20
+	 */
+	public List<SleepStatisticsQueryDto> plusPrevTime(List<SleepStatisticsQueryDto> sleeps) {
+		for (int i = 0; i < sleeps.size(); i++) {
+			if (sleeps.get(i).getEndTime().isBefore(sleeps.get(i).getCreatedTime())) {
+				sleeps.get(i).setCreatedTime(LocalTime.of(0, 0));
+			}
+		}
+
+		return sleeps;
+	}
+
+	/**
 	 * 총 시간 계산
 	 */
 	public Long sumSleepTime(List<SleepStatisticsQueryDto> sleeps) {
@@ -84,18 +128,5 @@ public class SleepStatisticsServiceImpl implements SleepStatisticsService{
 		}
 
 		return sleepTime;
-	}
-
-	/**
-	 * 퍼센트 계산하기 Long
-	 */
-	public Long getPercent(Long target, Long opponent) {
-		if (target == 0) return 0L;
-		else if (target >= opponent) {
-			return 100L;
-		}
-		else {
-			return target * 100 / opponent;
-		}
 	}
 }
