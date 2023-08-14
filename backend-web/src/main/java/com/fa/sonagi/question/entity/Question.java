7632d3438f8472cd1696131a38c3ad191c42d031
minseo0421 <k4496k@naver.com>
2023-08-14 14:10:17 +0900

    [S09P12B207-420] feat: 아기 정보 복구

--- backend-web/src/main/java/com/fa/sonagi/question/entity/Question.java
+++ backend-web/src/main/java/com/fa/sonagi/question/entity/Question.java
@@ -23,7 +23,7 @@ public class Question {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "question_id", nullable = false)
-	private Long id;
+	private Long questionID;
 
 	@Column(name = "user_id", nullable = false)
 	private Long userId;
--- frontend-web/src/pages/RestorePage/frontend-web/src/pages/RestorePage/RestorePage.tsx
+++ frontend-web/src/pages/RestorePage/frontend-web/src/pages/RestorePage/RestorePage.tsx
@@ -2,17 +2,17 @@ import { Background } from '@/components/atoms/Background/Background.styles';
 import orangeBackground from '@/assets/images/background-orange-to-blue.png';
 import { useEffect, useState } from 'react';
 import AdminBar from '@/components/molecules/AdminBar/AdminBar';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { DeletedBaby } from '@/types';
-import axios from 'axios';
+import { instance } from '@/apis/instance';
 
 const RestorePage = () => {
   const [deletedBabyList, setDeletedBabyList] = useState<DeletedBaby[]>([]);
   const [currentPost, setCurrentPost] = useState<DeletedBaby[]>([]);
 
   useEffect(() => {
-    axios
-      .get('http://localhost:8080/api/isDeleted')
+    instance
+      .get('/isDeleted')
       .then(response => {
         setDeletedBabyList([...response.data].reverse());
       })
@@ -25,17 +25,18 @@ const RestorePage = () => {
     setCurrentPost(deletedBabyList);
   }, [deletedBabyList]);
 
-  const handleRestore = (babyId: number) => {
-    axios
-      .put(`http://localhost:8080/api/isDeleted/${babyId}`)
-      .then(response => {
-        // 복구 성공 시 필요한 동작 수행
-        console.log('아기 복구 완료');
-        // 필요한 상태 업데이트 등을 수행할 수 있습니다.
-      })
-      .catch(error => {
-        console.error('복구 실패:', error);
-      });
+  const restoreBaby = (babyId: number) => {
+    if (window.confirm('해당 아기 정보를 복구하시겠습니까?')) {
+      instance
+        .put(`/isDeleted/${babyId}`)
+        .then(response => {
+          alert('아기 정보가 성공적으로 복구되었습니다.');
+          window.location.reload();
+        })
+        .catch(error => {
+          console.error('복구 실패:', error);
+        });
+    }
   };
 
   return (
@@ -52,6 +53,7 @@ const RestorePage = () => {
             <col width="20%" />
             <col width="10%" />
             <col width="20%" />
+            <col width="10%" />
           </colgroup>
 
           <thead>
@@ -62,6 +64,7 @@ const RestorePage = () => {
               <th>생년월일</th>
               <th>성별</th>
               <th>삭제된 날짜</th>
+              <th></th>
             </tr>
           </thead>
 
@@ -69,14 +72,13 @@ const RestorePage = () => {
             {currentPost.map((deletedBaby, index) => (
               <tr key={index}>
                 <td>{index + 1}</td>
-                <td>{deletedBaby.id}</td>
+                <td>{deletedBaby.babyId}</td>
                 <td>{deletedBaby.name}</td>
                 <td>{deletedBaby.birthDate}</td>
                 <td>{deletedBaby.gender}</td>
-                <td className="title"></td>
                 <td>{deletedBaby.deletedAt}</td>
                 <td>
-                  <button onClick={() => handleRestore(deletedBaby.id)}>
+                  <button onClick={() => restoreBaby(deletedBaby.babyId)}>
                     복구
                   </button>
                 </td>
