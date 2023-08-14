079ae56d409646fb37047e96af44943237c418fa
minseo0421 <k4496k@naver.com>
2023-08-15 01:11:37 +0900

    [S09P12B207-420] feat: 문의사항, 데이터 복구 디자인

--- frontend-web/src/pages/QuestionPage/QuestionDetailPage.tsx
+++ frontend-web/src/pages/QuestionPage/QuestionDetailPage.tsx
@@ -1,14 +1,17 @@
 import { useState, useEffect } from 'react';
-import { Background } from '@/components/atoms/Background/Background.styles';
-import orangeBackground from '@/assets/images/background-orange-to-blue.png';
-import AdminBar from '@/components/molecules/AdminBar/AdminBar';
-import { Link, useNavigate, Outlet, useParams } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 import { instance } from '@/apis/instance';
+import {
+  ContentContainer,
+  DetailContent,
+  DetailTitle,
+  ListContainer,
+  QuestionDetailContainer,
+  AdminButton,
+} from '../AdminPage/AdminPage.style';
 
 const QuestionDetailPage = () => {
   const questionID = useParams().id;
-  const navigate = useNavigate();
-  // state
   const [detailQuestionData, setDetailQuestionData] = useState<any>([]);
 
   useEffect(() => {
@@ -24,33 +27,24 @@ const QuestionDetailPage = () => {
   }, [questionID]);
 
   return (
-    <Background $background={orangeBackground}>
-      <div>문의사항</div>
-      <div className="question-detail">
-        <div className="question-wrapper">
-          <div className="question-header">
-            <p>번호 : {detailQuestionData.questionId}</p>
-          </div>
-          <div className="question-user">
-            <p>사용자 ID : {detailQuestionData.userId}</p>
-          </div>
-          <div className="question-title">
-            <p>제목 : {detailQuestionData.title}</p>
-          </div>
-          <div className="question-content">
-            <p>내용 : {detailQuestionData.content}</p>
-          </div>
-        </div>
-
-        <div className="grid-2">
-          <Link to={`/admin/question`}>
-            <button>목록</button>
-          </Link>
-        </div>
-      </div>
-      <AdminBar />
-      <Outlet />
-    </Background>
+    <ListContainer>
+      <ContentContainer>
+        <QuestionDetailContainer>
+          <a>사용자 ID : {detailQuestionData.userId}</a>
+          <a>등록일 : {detailQuestionData.createdAt}</a>
+        </QuestionDetailContainer>
+        <DetailTitle>
+          <p>제목 : {detailQuestionData.title}</p>
+        </DetailTitle>
+        <DetailContent>
+          <p>내용 : {detailQuestionData.content}</p>
+        </DetailContent>
+        <AdminButton>
+          <Link to={`/admin/question`}>목록</Link>
+        </AdminButton>
+        <Outlet />
+      </ContentContainer>
+    </ListContainer>
   );
 };
 
