12d247cc6565af5ddceea22a5f0f1b03dab61952
minseo0421 <k4496k@naver.com>
2023-08-17 22:51:03 +0900

    [S09P12B207-424] feat: 검진 정보 수정 및 데이터 추가

--- backend-web/src/main/java/com/fa/sonagi/immunization/entity/Checkup.java
+++ backend-web/src/main/java/com/fa/sonagi/immunization/entity/Checkup.java
@@ -32,6 +32,27 @@ public class Checkup {
 	@Column(name = "end_date")
 	private int endDate;
 
-	@Column(name = "content", length = 300)
-	private String content;
+	@Column(name = "content1", length = 1000)
+	private String content1;
+
+	@Column(name = "title1", length = 200)
+	private String title1;
+
+	@Column(name = "content2", length = 1000)
+	private String content2;
+
+	@Column(name = "title2", length = 200)
+	private String title2;
+
+	@Column(name = "content3", length = 1000)
+	private String content3;
+
+	@Column(name = "title3", length = 200)
+	private String title3;
+
+	@Column(name = "content4", length = 1000)
+	private String content4;
+
+	@Column(name = "title4", length = 200)
+	private String title4;
 }
