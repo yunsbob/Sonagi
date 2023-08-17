e694e98e38555d4c35cbd9c20793178907985902
minseo0421 <k4496k@naver.com>
2023-08-17 20:57:17 +0900

    [S09P12B207-421] feat: faq 카테고리 조회 추가

--- backend-web/src/main/java/com/fa/sonagi/faq/repository/FAQRepository.java
+++ backend-web/src/main/java/com/fa/sonagi/faq/repository/FAQRepository.java
@@ -1,8 +1,11 @@
 package com.fa.sonagi.faq.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fa.sonagi.faq.entity.FAQ;
 
 public interface FAQRepository extends JpaRepository<FAQ, Long> {
+	List<FAQ> findByCategory(String category);
 }
